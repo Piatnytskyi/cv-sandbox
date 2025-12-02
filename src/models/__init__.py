6d010455fa413090caf399330e37f545c9191a88
Vitalii Piatnytskyi <v.v.piatnytskyi@gmail.com>
2025-12-02 18:52:49 +0200

    Fixed init includes for models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,3 @@
 from .image import Image
-from .data_generator import DataSource, DataGenerator
-from .image_data_generator import HDF5ImageSource, ImageDataGenerator
 
-__all__ = ["Image", "DataSource", "DataGenerator", "HDF5ImageSource", "ImageDataGenerator"]
+__all__ = ["Image"]
